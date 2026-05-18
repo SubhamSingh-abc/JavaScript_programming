4dcb53dc77ab68e7a9bb8e773f3f8a860fb69884
Subham Singh <subhamsinghcol@gmail.com>
2026-05-18 19:12:47 +0530

    array

---
+++ array1.js
@@ -0,0 +1,5 @@
+let arr = [10,20,30,40,50];         // initialising array
+console.log(arr);                   // printing whole array
+console.log(arr[1]);                // accessing value
+arr[1] = 100;                       // modifying value
+console.log(arr[1]);                
