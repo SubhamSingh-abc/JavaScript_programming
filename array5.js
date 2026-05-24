b186d4f933f88bd52280a382824bd23705955885
Subham Singh <subhamsinghcol@gmail.com>
2026-05-24 09:16:54 +0530

    array

---
+++ array5.js
@@ -0,0 +1,8 @@
+// array.splice(start, deleteCount, item1 , item2 , ...);
+// splice return removed array
+let arr = [10,20,null,true,false,"SUBHAM","","SINGH"];
+console.log(arr);
+arr.splice(2,6);
+console.log(arr);
+arr.splice(2,0,30,40,50);
+console.log(arr);
