afede67c42b72caad53c196a33b4c52fa91ae47b
Subham Singh <subhamsinghcol@gmail.com>
2026-06-21 13:09:27 +0530

    Add files via upload

---
+++ strings4.js
@@ -0,0 +1,11 @@
+let str = "Subham Singh";       
+let lower_str = str.toLowerCase();
+let upper_str = str.toUpperCase();
+console.log(str);
+console.log(upper_str);
+console.log(lower_str);
+
+
+// String methods don’t change the original — they return a new modified string
+// str.toLowerCase();
+// str.toUpperCase();
