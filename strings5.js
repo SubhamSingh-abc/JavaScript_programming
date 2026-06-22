dfc23e122121eb734e0e897438f085e7dd2ea795
Subham Singh <subhamsinghcol@gmail.com>
2026-06-22 12:40:24 +0530

    Add files via upload

---
+++ strings5.js
@@ -0,0 +1,10 @@
+// str.trim();   (Removes spaces from : Start , End)
+// trim() removes: spaces " " , tabs \t , newlines \n
+// str.trimEnd();     (trims from end)
+// str.trimStart();   (trim from start)
+let str = "   subham ";
+let after_trim_str = str.trim();
+console.log(str);
+console.log(after_trim_str);
+console.log(str.trimStart());      // trim from start
+console.log(str.trimEnd());        // trim from end
