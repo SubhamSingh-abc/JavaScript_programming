d3f4a21be0bd3e81f03042472fb50738dbb62081
Subham Singh <subhamsinghcol@gmail.com>
2026-06-25 10:46:36 +0530

    Add files via upload

---
+++ strings7.js
@@ -0,0 +1,10 @@
+let str = "aaa";
+console.log(str);
+console.log(str.replace("a","b"));
+console.log(str.replaceAll("a","b"));
+
+// real use : 
+// let ex_str = "a b c d e f ";
+// console.log(ex_str);
+// ex_str = ex_str.replaceAll(" ","");
+// console.log(ex_str);
