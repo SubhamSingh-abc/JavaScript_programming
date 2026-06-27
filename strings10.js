05cc517391d1e7989f7414dc3bc4ebdf980f8b17
Subham Singh <subhamsinghcol@gmail.com>
2026-06-27 22:11:02 +0530

    Add files via upload

---
+++ strings10.js
@@ -0,0 +1,5 @@
+let str = "google and microsoft are top tech players , both google and microsoft are part of MAANG";
+console.log(str.indexOf("google"));
+console.log(str.indexOf("google",10));
+console.log(str.lastIndexOf("google"));
+// if not found index for both will be -1
